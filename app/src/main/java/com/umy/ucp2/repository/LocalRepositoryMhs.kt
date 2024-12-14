ed8612b001bf52f958d9e3d2347d60a2a8508966
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-14 23:43:28 +0700

    Menambahkan Method getAllMhs pada LocalRepositoryMhs untuk mendapatkan semua data

--- app/src/main/java/com/umy/ucp2/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/umy/ucp2/repository/LocalRepositoryMhs.kt
@@ -2,6 +2,7 @@ package com.umy.ucp2.repository
 
 import com.umy.ucp2.data.dao.MahasiswaDao
 import com.umy.ucp2.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
@@ -9,4 +10,6 @@ class LocalRepositoryMhs (
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
         mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+
+    fun getAllMhs() : Flow<List<Mahasiswa>>
 }
