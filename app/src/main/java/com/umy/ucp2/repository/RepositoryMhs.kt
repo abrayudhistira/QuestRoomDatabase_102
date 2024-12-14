1ad535fd979b444c2de54d69057897298e5c052e
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-14 23:37:52 +0700

    Menambahkan Method getAllMhs pada RepositoryMhs untuk mendapatkan semua data

--- app/src/main/java/com/umy/ucp2/repository/RepositoryMhs.kt
+++ app/src/main/java/com/umy/ucp2/repository/RepositoryMhs.kt
@@ -1,7 +1,10 @@
 package com.umy.ucp2.repository
 
 import com.umy.ucp2.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+
+    fun getAllMhs() : Flow<List<Mahasiswa>>
 }
