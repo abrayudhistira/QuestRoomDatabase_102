42508e4182211e22a519f6030df2e81410a2f664
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-15 00:53:33 +0700

    Membuat File HomeMhsViewModel dan membuat class

---
+++ app/src/main/java/com/umy/ucp2/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,10 @@
+package com.umy.ucp2.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.umy.ucp2.repository.RepositoryMhs
+
+class HomeMhsViewModel (
+    private val repositoryMhs : RepositoryMhs
+) : ViewModel() {
+
+}
