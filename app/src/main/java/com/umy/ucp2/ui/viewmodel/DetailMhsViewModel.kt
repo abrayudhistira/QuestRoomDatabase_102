2b3c8e3c79c8f705b3e2c1175caa8079631ea935
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-15 20:03:49 +0700

    Menginisiasi variabel dari Repository

--- app/src/main/java/com/umy/ucp2/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/umy/ucp2/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,5 +1,12 @@
 package com.umy.ucp2.ui.viewmodel
 
-class DetailMhsViewModel (
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.umy.ucp2.repository.RepositoryMhs
 
-)
+class DetailMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+): ViewModel() {
+
+}
