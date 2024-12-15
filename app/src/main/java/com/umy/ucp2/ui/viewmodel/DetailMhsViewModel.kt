b25ffe669faf8e82902bf2b3ec7a227790a05d87
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-15 20:05:12 +0700

    Menginisiasi variabel _nim

--- app/src/main/java/com/umy/ucp2/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/umy/ucp2/ui/viewmodel/DetailMhsViewModel.kt
@@ -3,10 +3,12 @@ package com.umy.ucp2.ui.viewmodel
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.umy.ucp2.repository.RepositoryMhs
+import com.umy.ucp2.ui.navigation.DestinasiDetail
 
 class DetailMhsViewModel (
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs,
 ): ViewModel() {
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
 
 }
