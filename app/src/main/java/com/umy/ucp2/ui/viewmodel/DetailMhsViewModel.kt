65e8a36c23f2879dae9af0edc875414b220bc016
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-15 20:10:24 +0700

    membuat variabel detailUiState, membuat event, dan memasukkan repository

--- app/src/main/java/com/umy/ucp2/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/umy/ucp2/ui/viewmodel/DetailMhsViewModel.kt
@@ -2,8 +2,15 @@ package com.umy.ucp2.ui.viewmodel
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.umy.ucp2.repository.RepositoryMhs
 import com.umy.ucp2.ui.navigation.DestinasiDetail
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
 class DetailMhsViewModel (
     savedStateHandle: SavedStateHandle,
@@ -11,4 +18,26 @@ class DetailMhsViewModel (
 ): ViewModel() {
     private val _nim: String = checkNotNull(savedStateHandle[DestinasiDetail.NIM])
 
-}
+    val detailUIState: StateFlow<DetailUiState> = repositoryMhs.getMhs(_nim)
+        .filterNotNull()
+        .map {
+            DetailUiState ()
+            detailUiEvent = it.toDetailUiEvent(),
+            isLoading = false,
+        }
+        .onStart {
+            emit(
+                DetailUiState (
+                    isLoading = true,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan",
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(2000),
+            initialValue = DetailUiState (
+                isLoading = true,
+            ),
+        )
