c906492cb821aeb9798f17dff11eb4c9821e9b4d
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-15 20:53:52 +0700

    Membuat variable untuk memasukkan dari alamat navigasi

--- app/src/main/java/com/umy/ucp2/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/umy/ucp2/ui/viewmodel/UpdateMhsViewModel.kt
@@ -3,8 +3,12 @@ package com.umy.ucp2.ui.viewmodel
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.umy.ucp2.repository.RepositoryMhs
 import com.umy.ucp2.ui.navigation.DestinasiUpdate
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class UpdateMhsViewModel (
     savedStateHandle: SavedStateHandle,
@@ -13,4 +17,13 @@ class UpdateMhsViewModel (
     var updateUiState by mutableStateOf(MhsUIState())
         private set
     private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUiState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
 }
