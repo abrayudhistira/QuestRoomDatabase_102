0fce0bcbbd0a3a7ebbe4d9b78a277d90c0dad91c
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2024-12-15 20:46:52 +0700

    Membuat Class UpdateMhsViewModel

--- app/src/main/java/com/umy/ucp2/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/umy/ucp2/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,2 +1,16 @@
 package com.umy.ucp2.ui.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.umy.ucp2.repository.RepositoryMhs
+import com.umy.ucp2.ui.navigation.DestinasiUpdate
+
+class UpdateMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+): ViewModel() {
+    var updateUiState by mutableStateOf(MhsUIState())
+        private set
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+}
